a9891a14818796f4166df35ccbaeb8a7d64d61b6
Majed Biazid <majedbiazid@gmail.com>
2025-09-16 11:59:15 +0300

    chore: font setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-primary-600">autobia test</h1>
+      <h1 className="text-primary-600  text-4xl"></h1>
     </div>
   );
 }
